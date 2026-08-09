2e35cd68f976f3ea64247fe8de9d0f6108d17631
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2026-08-09 21:52:00 +0530

    DAA work

--- WEEK 1/Que5.c
+++ WEEK 1/Que5.c
@@ -1,43 +1,65 @@
+//Write a program to multiply two matrices. Before multiplication, verify whether the given matrices are compatible.
+
 #include <stdio.h>
+#define n 50
 
 int main()
 {
-    int a[10][10], b[10][10], c[10][10];
-    int r1, c1, r2, c2, i, j, k;
+    int a[n][n], b[n][n], c[n][n];
+    int r1, c1, r2, c2;
+    int i, j, k, sum;
 
+    printf("Enter the number of rows and columns of first matrix: ");
     scanf("%d%d", &r1, &c1);
+
+    printf("Enter the number of rows and columns of second matrix: ");
     scanf("%d%d", &r2, &c2);
 
-    if(c1 != r2)
+    if (c1 != r2)
     {
-        printf("Not possible");
+        printf("Matrices are not compatible for multiplication.\n");
         return 0;
     }
 
-    for(i=0; i<r1; i++)
-        for(j=0; j<c1; j++)
-            scanf("%d", &a[i][j]);
-
-    for(i=0; i<r2; i++)
-        for(j=0; j<c2; j++)
-            scanf("%d", &b[i][j]);
-
-    for(i=0; i<r1; i++)
+    printf("Enter the elements of first matrix:\n");
+    for (i = 0; i < r1; i++)
     {
-        for(j=0; j<c2; j++)
+        for (j = 0; j < c1; j++)
         {
-            c[i][j] = 0;
-
-            for(k=0; k<c1; k++)
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+            scanf("%d", &a[i][j]);
         }
     }
 
-    for(i=0; i<r1; i++)
+    printf("Enter the elements of second matrix:\n");
+    for (i = 0; i < r2; i++)
     {
-        for(j=0; j<c2; j++)
-            printf("%d ", c[i][j]);
+        for (j = 0; j < c2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
 
+    // Matrix multiplication
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            sum = 0;
+            for (k = 0; k < c1; k++)
+            {
+                sum += a[i][k] * b[k][j];
+            }
+            c[i][j] = sum;
+        }
+    }
+
+    printf("Resultant matrix after multiplication:\n");
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
         printf("\n");
     }
 
