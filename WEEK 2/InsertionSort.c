acc26732e73d1fe9766745d5665e63e46311e4af
Abhishek Bansal <abhishekbansal2806@gmail.com>
2026-08-23 11:49:55 +0530

    DAA work

--- WEEK 2/InsertionSort.c
+++ WEEK 2/InsertionSort.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-    int a[100], n, i, j, key;
 
-    printf("Enter size: ");
-    scanf("%d", &n);
+    static int arr[10000];
 
-    printf("Enter elements:\n");
-    for(i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+    int sizes[] = {1000, 2000, 4000, 6000, 8000, 10000};
+    int numSizes = 6;
 
-    for(i = 1; i < n; i++) {
+    printf("Insertion Sort Time Complexity Analysis\n");
+    printf("---------------------------------------\n");
 
-        key = a[i];
-        j = i - 1;
+    for (int s = 0; s < numSizes; s++) {
 
-        while(j >= 0 && a[j] > key) {
-            a[j + 1] = a[j];
-            j--;
+        int n = sizes[s];
+
+        // Reverse order array
+        for (int i = 0; i < n; i++)
+            arr[i] = n - i;
+
+        clock_t start = clock();
+
+        // Insertion Sort
+        for (int i = 1; i < n; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
         }
 
-        a[j + 1] = key;
+        clock_t end = clock();
+
+        double time_taken =
+            (double)(end - start) / CLOCKS_PER_SEC;
+
+        printf("Input Size = %d, Time = %f seconds\n",
+               n, time_taken);
     }
 
-    printf("Sorted array:\n");
-    for(i = 0; i < n; i++)
-        printf("%d ", a[i]);
+    printf("\nTime Complexity: O(n^2)\n");
+    printf("Space Complexity: O(1)\n");
 
     return 0;
 }
