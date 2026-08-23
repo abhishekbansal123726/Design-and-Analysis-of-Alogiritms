b328ababa875a22a7a8b31c76f124183f16917e4
Abhishek Bansal <abhishekbansal2806@gmail.com>
2026-08-23 11:48:28 +0530

    DAA work

--- WEEK 2/ linearsearch.c
+++ WEEK 2/ linearsearch.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
+#include <time.h>
 
-int main()
-{
-    int a[5] = {10, 20, 30, 40, 50};
-    int key, i, found = 0;
+int main() {
+    static int arr[1000000];
 
-    printf("Enter element to search: ");
-    scanf("%d", &key);
+    int sizes[] = {10000, 50000, 100000, 500000, 1000000};
+    int numSizes = 5;
 
-    for(i = 0; i < 5; i++)
-    {
-        if(a[i] == key)
-        {
-            printf("Element found at index %d", i);
-            found = 1;
-            break;
+    printf("Linear Search Time Complexity Analysis\n");
+    printf("-------------------------------------\n");
+
+    for (int s = 0; s < numSizes; s++) {
+
+        int n = sizes[s];
+        int key = n - 1;
+        int found = 0;
+
+        // Create array
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+
+        clock_t start = clock();
+
+        // Run Linear Search many times
+        for (int k = 0; k < 100; k++) {
+
+            for (int i = 0; i < n; i++) {
+
+                if (arr[i] == key) {
+                    found = 1;
+                    break;
+                }
+            }
         }
+
+        clock_t end = clock();
+
+        double time_taken =
+            (double)(end - start) / CLOCKS_PER_SEC;
+
+        printf("Input Size = %d, Time = %f seconds\n",
+               n, time_taken);
+
+        if (found)
+            printf("Element Found\n\n");
     }
 
-    if(found == 0)
-        printf("Element not found");
+    printf("Time Complexity: O(n)\n");
+    printf("Space Complexity: O(1)\n");
 
     return 0;
 }
